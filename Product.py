276a0dc5bc231f1d9abc4b2f665b702345e26792
TarampikosAndreas <a.tara22@hotmail.com>
2021-06-07 18:57:09 +0300

    added price to product

--- Product.py
+++ Product.py
@@ -2,6 +2,10 @@
 class Product:
     def __init__(self, name):
         self.name = name
+        self.price=0
 
     def __str__(self):
         return self.name
+
+    def setPrice (self,price):
+        self.price=price
